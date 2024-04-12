723f6eced2ef64174e1f55108999816a66ad8b5f
Jean-Philippe Brucker <jean-philippe@linaro.org>
2024-04-12 09:39:54 +0100

    hypervisor: kvm: Handle memory fault exit
    A new KVM exit reason informs the VMM that it should change the state of
    some pages. Add a new vm_ops function to handle this exit.
    
    Signed-off-by: Jean-Philippe Brucker <jean-philippe@linaro.org>

--- hypervisor/src/lib.rs
+++ hypervisor/src/lib.rs
@@ -58,8 +58,8 @@ pub use kvm::aarch64;
 #[cfg(all(feature = "kvm", target_arch = "riscv64"))]
 pub use kvm::{AiaState, riscv64};
 pub use vm::{
-    DataMatch, HypervisorVmError, InterruptSourceConfig, LegacyIrqSourceConfig, MsiIrqSourceConfig,
-    Vm, VmOps,
+    DataMatch, HypervisorVmError, InterruptSourceConfig, LegacyIrqSourceConfig, MemoryAttribute,
+    MemoryFaultType, MsiIrqSourceConfig, Vm, VmOps,
 };
 
 pub use crate::hypervisor::{Hypervisor, HypervisorError};
